4607bdaa105fbd19920197ab63c660ad2c3ba6c8
cllorador <42364721+cllorador@users.noreply.github.com>
2018-10-12 19:01:08 +0800

    v2.1
    changed data source of site/lob data

--- mnl_data_preparation_v2.R
+++ mnl_data_preparation_v2.R
@@ -26,8 +26,10 @@ saveRDS(token,"googlesheets_token_1.rds")
 gs_auth(token="googlesheets_token_1.rds")
 
 # LOB level
-lob_level <- gs_title("MNL COE - Project Lighthouse LOB Raw (MDS)")
+lob_level <- gs_title("MNL COE Outflows")
 lob_data <- gs_read(lob_level)
+
+
 
 #lob_data$wb <- as.Date(lob_data$wb,"%Y/%m/%d")
 
@@ -36,32 +38,27 @@ lob_data <- gs_read(lob_level)
 site_data<-lob_data[lob_data$line_of_business=="SITE",]
 #colnames(site_data)
 site_data_diff<-sqldf("Select 
-                      performance_week as Week,
+                      week as Week,
                       'COE-UBR-MNL' as Site,
-                      (bliss_solves+liveops_solves+jira_solves+zd_solves) as Actual_Solved,
-                      (csat_survey + liveops_csat_score) as 'Actual_Responses',
+                      (bliss_solves+jira_solves) as Actual_Solved,
+                      (csat_score) as 'Actual_Responses',
                       round(Target_CSAT,2) as Target_CSAT,
                       round(CSAT,2) as Actual_CSAT,
-                      round((CSAT-Target_CSAT),2) as 'CSAT_Delta',
-                      round(Target_R2,2) as Target_R2,
-                      round(R2,2) as Actual_R2,
-                      round((R2-Target_R2),2) as 'R2_Delta',
-                      round(Target_APH,2) as 'Target_APH',
-                      round(APH,2) as Actual_APH,
-                      round((APH-Target_APH),2) as 'APH_Delta',
-                      round(Target_Resolution_Efficiency,2) as 'Target_Resolution_Efficiency',
-                      round(resolution_efficiency,2) as Actual_Resolution_Efficiency,
-                      round((resolution_efficiency-Target_resolution_efficiency),2) as 'Resolution_Efficiency_Delta',
-                      round(Target_AHT,2) as Target_Liveops_AHT,
-                      round(AHT,2) as Actual_Liveops_AHT,
-                      round((AHT-Target_AHT),2) as 'Liveops_AHT_Delta',
-                      round(productive_hours,2) as 'productive_hours',
-                      efficiency_solve_actions,
-                      efficiency_work_actions,
-                      (csat_survey + liveops_csat_survey) as 'CSAT_Surveys',
-                      qa_total,
-                      qa_survey,
-                      round(liveops_hours,2) as 'liveops_hours'
+                      round(CSAT-Target_CSAT,2) as 'CSAT_Delta',
+                      0 as 'Target_R2',
+                      0 as 'Actual_R2',
+                      0 as 'R2_Delta',
+                      round(Target_TPH,2) as Target_TPH,
+                      round(TPH,2) as Actual_TPH,
+                      round((TPH-Target_TPH),2) as 'TPH_Delta',
+                      round(Target_res_eff,2) as Target_Resolution_Efficiency,
+                      round(res_eff,2) as Actual_Resolution_Efficiency,
+                      round((res_eff-Target_res_eff),2) as 'Resolution_Efficiency_Delta',
+                      round(bliss_handle_time,2) as 'Bliss_Handle_Time',
+                      0 as 'Target_Liveops_AHT',
+                      0 as 'Actual_Liveops_AHT',
+                      0 as 'Liveops_AHT_Delta'
+                      
                       from site_data"
 )
 
@@ -71,15 +68,13 @@ site_data_diff$Week<-as.Date(site_data_diff$Week)
 site_data<-lob_data[lob_data$line_of_business=="SITE",]
 
 site_solved_data_diff<-sqldf("Select 
-                            
-                            performance_week as Week,
-                            'COE-UBR-MNL' as Site,
-                            bliss_solves as Bliss_Solved,
-                            liveops_solves as Liveops_Solved,
-                            jira_solves as JIRA_Solved,
-                            zd_solves as Zendesk_Solved
-                            
-                            from lob_data")
+                             
+                             week as Week,
+                             'COE-UBR-MNL' as Site,
+                             bliss_solves as Bliss_Solved,
+                             jira_solves as JIRA_Solved
+                             
+                             from lob_data")
 
 site_solved_data_diff$Week<-as.Date(site_solved_data_diff$Week)
 
@@ -90,33 +85,27 @@ site_solved_data_diff$Week<-as.Date(site_solved_data_diff$Week)
 lob_data<-lob_data[!lob_data$line_of_business %in% c("SITE"),]
 
 lob_data_diff<-sqldf("Select 
-                      
-                     performance_week as Week,
+                     week as Week,
                      line_of_business as LOB,
-                      (bliss_solves+liveops_solves+jira_solves+zd_solves) as Actual_Solved,
-                      (csat_survey + liveops_csat_score) as 'Actual_Responses',
+                     (bliss_solves+jira_solves) as Actual_Solved,
+                     (csat_score) as 'Actual_Responses',
                      round(Target_CSAT,2) as Target_CSAT,
                      round(CSAT,2) as Actual_CSAT,
-                     round((CSAT-Target_CSAT),2) as 'CSAT_Delta',
-                     round(Target_R2,2) as Target_R2,
-                     round(R2,2) as Actual_R2,
-                     round((R2-Target_R2),2) as 'R2_Delta',
-                     round(Target_APH,2) as 'Target_APH',
-                     round(APH,2) as Actual_APH,
-                     round((APH-Target_APH),2) as 'APH_Delta',
-                     round(Target_Resolution_Efficiency,2) as 'Target_Resolution_Efficiency',
-                     round(resolution_efficiency,2) as Actual_Resolution_Efficiency,
-                     round((resolution_efficiency-Target_resolution_efficiency),2) as 'Resolution_Efficiency_Delta',
-                     round(Target_AHT,2) as Target_Liveops_AHT,
-                     round(AHT,2) as Actual_Liveops_AHT,
-                     round((AHT-Target_AHT),2) as 'Liveops_AHT_Delta',
-                     round(productive_hours,2) as 'productive_hours',
-                     efficiency_solve_actions,
-                     efficiency_work_actions,
-                     (csat_survey + liveops_csat_survey) as 'CSAT_Surveys',
-                     qa_total,
-                     qa_survey,
-                     round(liveops_hours,2) as 'liveops_hours'
+                     round(CSAT-Target_CSAT,2) as 'CSAT_Delta',
+                     0 as 'Target_R2',
+                     0 as 'Actual_R2',
+                     0 as 'R2_Delta',
+                     round(Target_TPH,2) as Target_TPH,
+                     round(TPH,2) as Actual_TPH,
+                     round((TPH-Target_TPH),2) as 'TPH_Delta',
+                     round(Target_res_eff,2) as Target_Resolution_Efficiency,
+                     round(res_eff,2) as Actual_Resolution_Efficiency,
+                     round((res_eff-Target_res_eff),2) as 'Resolution_Efficiency_Delta',
+                     round(bliss_handle_time,2) as 'Bliss_Handle_Time',
+                     0 as 'Target_Liveops_AHT',
+                     0 as 'Actual_Liveops_AHT',
+                     0 as 'Liveops_AHT_Delta'
+                     
                      from lob_data"
 )
 lob_data_diff$Week<-as.Date(lob_data_diff$Week)
@@ -125,15 +114,13 @@ lob_data_diff$Week<-as.Date(lob_data_diff$Week)
 lob_data<-lob_data[!lob_data$line_of_business %in% c("SITE"),]
 
 lob_solved_data_diff<-sqldf("Select 
-
-                    performance_week as Week,
-                    line_of_business as LOB,
-                    bliss_solves as Bliss_Solved,
-                    liveops_solves as Liveops_Solved,
-                    jira_solves as JIRA_Solved,
-                    zd_solves as Zendesk_Solved
-
-                    from lob_data")
+                            
+                            week as Week,
+                            line_of_business as Site,
+                            bliss_solves as Bliss_Solved,
+                            jira_solves as JIRA_Solved
+                            
+                            from lob_data")
 
 lob_solved_data_diff$Week<-as.Date(lob_solved_data_diff$Week)
 
@@ -160,23 +147,23 @@ agent_data$efficiency_work_actions<-as.numeric(agent_data$efficiency_work_action
 
 
 tl_data<- sqldf(" select 
-                     performance_week as Week, 
-                     act_team_lead_email as TL,
-                     sum(bliss_solves+liveops_solves+jira_solves+zd_solves) as Actual_Solved,
-                     csat_survey as Actual_Responses,
-                     avg(Target_CSAT) as Target_CSAT,
-                     round(csat_score/csat_survey,2) as Actual_CSAT,
-                     avg(Target_R2) as Target_R2,
-                     sum(qa_total)/sum(qa_survey) as Actual_R2,
-                     avg(Target_APH) as Target_APH,
-                     sum(bliss_solves)/sum(productive_hours) as Actual_APH,
-                     avg(Target_Resolution_Efficiency) as Target_Resolution_Efficiency,
-                     sum(bliss_solves)/sum(efficiency_work_actions) as Actual_Resolution_Efficiency,
-                     avg(Target_AHT) as Target_Liveops_AHT,
-                     sum(liveops_solves)/sum(liveops_hours) as Actual_Liveops_AHT
-                      
-                     from agent_data
-                     group by 1,2")
+                performance_week as Week, 
+                act_team_lead_email as TL,
+                sum(bliss_solves+liveops_solves+jira_solves+zd_solves) as Actual_Solved,
+                csat_survey as Actual_Responses,
+                avg(Target_CSAT) as Target_CSAT,
+                round(csat_score/csat_survey,2) as Actual_CSAT,
+                avg(Target_R2) as Target_R2,
+                sum(qa_total)/sum(qa_survey) as Actual_R2,
+                avg(Target_APH) as Target_APH,
+                sum(bliss_solves)/sum(productive_hours) as Actual_APH,
+                avg(Target_Resolution_Efficiency) as Target_Resolution_Efficiency,
+                sum(bliss_solves)/sum(efficiency_work_actions) as Actual_Resolution_Efficiency,
+                avg(Target_AHT) as Target_Liveops_AHT,
+                sum(liveops_solves)/sum(liveops_hours) as Actual_Liveops_AHT
+                
+                from agent_data
+                group by 1,2")
 
 tl_data_diff<- sqldf("select 
                      Week, 
@@ -198,9 +185,9 @@ tl_data_diff<- sqldf("select
                      round(Target_Liveops_AHT,2) as Target_Liveops_AHT,
                      round(Actual_Liveops_AHT,2) as Actual_Liveops_AHT,
                      round((Actual_Liveops_AHT-Target_Liveops_AHT),2) as 'Liveops_AHT_Delta'
-                      
+                     
                      from tl_data
-                    ")
+                     ")
 
 tl_data_diff$Week<-as.Date(tl_data_diff$Week)
 
@@ -216,39 +203,39 @@ tl_solved_data_diff<- sqldf("select
 tl_solved_data_diff$Week<-as.Date(tl_solved_data_diff$Week)
 
 agent_data_diff<-sqldf(" Select
-                     performance_week as Week, 
-                     agent_email as Agent_Email, 
-                     (bliss_solves+liveops_solves+jira_solves+zd_solves) as Actual_Solved,
-                     csat_survey as Actual_Responses,
-                     round(Target_CSAT,2) as Target_CSAT,
-                     round(CSAT,2) as Actual_CSAT,
-                     round((CSAT - Target_CSAT),2) as CSAT_Delta,
-                     round(Target_R2,2) as Target_R2,
-                     round(R2,2) as Actual_R2,
-                     round((R2 - Target_R2),2) as R2_Delta,
-                     round(Target_APH,2) as Target_APH,
-                     round(APH,2) as Actual_APH,
-                     round((APH - Target_APH),2) as APH_Delta,
-                     round(Target_Resolution_Efficiency,2)  as Target_Resolution_Efficiency,
-                     round(resolution_efficiency,2) as Actual_Resolution_Efficiency,
-                     round((resolution_efficiency-Target_Resolution_Efficiency),2) as Resolution_Efficiency_Delta,
-                     round(Target_AHT,2) as Target_Liveops_AHT,
-                     round(AHT,2) as Actual_Liveops_AHT,
-                     round((AHT-Target_AHT),2) as 'Liveops_AHT_Delta'
-                      
-                     from agent_data
-                    ")
+                       performance_week as Week, 
+                       agent_email as Agent_Email, 
+                       (bliss_solves+liveops_solves+jira_solves+zd_solves) as Actual_Solved,
+                       csat_survey as Actual_Responses,
+                       round(Target_CSAT,2) as Target_CSAT,
+                       round(CSAT,2) as Actual_CSAT,
+                       round((CSAT - Target_CSAT),2) as CSAT_Delta,
+                       round(Target_R2,2) as Target_R2,
+                       round(R2,2) as Actual_R2,
+                       round((R2 - Target_R2),2) as R2_Delta,
+                       round(Target_APH,2) as Target_APH,
+                       round(APH,2) as Actual_APH,
+                       round((APH - Target_APH),2) as APH_Delta,
+                       round(Target_Resolution_Efficiency,2)  as Target_Resolution_Efficiency,
+                       round(resolution_efficiency,2) as Actual_Resolution_Efficiency,
+                       round((resolution_efficiency-Target_Resolution_Efficiency),2) as Resolution_Efficiency_Delta,
+                       round(Target_AHT,2) as Target_Liveops_AHT,
+                       round(AHT,2) as Actual_Liveops_AHT,
+                       round((AHT-Target_AHT),2) as 'Liveops_AHT_Delta'
+                       
+                       from agent_data
+                       ")
 agent_data_diff$Week<-as.Date(agent_data_diff$Week)
 
 agent_solved_data_diff<- sqldf("select
-                            performance_week as Week, 
-                            agent_email as Agent_Email,
-                            bliss_solves as Bliss_Solved,
-                            liveops_solves as Liveops_Solved,
-                            jira_solves as JIRA_Solved,
-                            zd_solves as Zendesk_Solved
-                            
-                            from agent_data")
+                               performance_week as Week, 
+                               agent_email as Agent_Email,
+                               bliss_solves as Bliss_Solved,
+                               liveops_solves as Liveops_Solved,
+                               jira_solves as JIRA_Solved,
+                               zd_solves as Zendesk_Solved
+                               
+                               from agent_data")
 agent_solved_data_diff$Week<-as.Date(agent_solved_data_diff$Week)
 
 
@@ -265,40 +252,40 @@ regional_data$shrinkage_actuals<-as.numeric(regional_data$shrinkage_actuals)
 regional_data$shrinkage_forecast<-as.numeric(regional_data$shrinkage_forecast)
 
 BOP_data<- sqldf(" select 
-                wb as Week, 
-                lob_tag as LOB,
-                inflow_actuals as Actual_Inflows,
-                inflow_forecast as Forecasted_Inflows,
-                outflow_actuals as Actual_Outflows,
-                outflow_forecast as Forecasted_Outflows,
-                round(sla_6h,2) as 'first_response_6H',
-                round(sla_24h,2) as 'first_response_24H',
-                headcount_actuals as Actual_Headcount,
-                headcount_forecast as Forecasted_Headcount,
-                round(shrinkage_actuals,2) as Actual_Shrinkage,
-                round(shrinkage_forecast,2) as Forecasted_Shrinkage
-                
-                from regional_data")
+                 wb as Week, 
+                 lob_tag as LOB,
+                 inflow_actuals as Actual_Inflows,
+                 inflow_forecast as Forecasted_Inflows,
+                 outflow_actuals as Actual_Outflows,
+                 outflow_forecast as Forecasted_Outflows,
+                 round(sla_6h,2) as 'first_response_6H',
+                 round(sla_24h,2) as 'first_response_24H',
+                 headcount_actuals as Actual_Headcount,
+                 headcount_forecast as Forecasted_Headcount,
+                 round(shrinkage_actuals,2) as Actual_Shrinkage,
+                 round(shrinkage_forecast,2) as Forecasted_Shrinkage
+                 
+                 from regional_data")
 
 BOP_data_diff<- sqldf("select 
-                Week, 
-                LOB,
-                Actual_Inflows,
-                Forecasted_Inflows,
-                (Actual_Inflows - Forecasted_Inflows) as Inflows_Delta,
-                Actual_Outflows,
-                Forecasted_Outflows,
-                (Actual_Outflows - Forecasted_Outflows) as Outflows_Delta,
-                first_response_6H,
-                first_response_24H,
-                Actual_Headcount,
-                Forecasted_Headcount,
-                (Actual_Headcount - Forecasted_Headcount) as Headcount_Delta,
-                Actual_Shrinkage,
-                Forecasted_Shrinkage,
-                (Actual_Shrinkage - Forecasted_Shrinkage) as Shrinkage_Delta
-                
-                from BOP_data")
+                      Week, 
+                      LOB,
+                      Actual_Inflows,
+                      Forecasted_Inflows,
+                      (Actual_Inflows - Forecasted_Inflows) as Inflows_Delta,
+                      Actual_Outflows,
+                      Forecasted_Outflows,
+                      (Actual_Outflows - Forecasted_Outflows) as Outflows_Delta,
+                      first_response_6H,
+                      first_response_24H,
+                      Actual_Headcount,
+                      Forecasted_Headcount,
+                      (Actual_Headcount - Forecasted_Headcount) as Headcount_Delta,
+                      Actual_Shrinkage,
+                      Forecasted_Shrinkage,
+                      (Actual_Shrinkage - Forecasted_Shrinkage) as Shrinkage_Delta
+                      
+                      from BOP_data")
 
 BOP_data_diff$Week<-as.Date(BOP_data_diff$Week)
 inflows_data<- sqldf ("select
@@ -322,7 +309,7 @@ outflows_data<- sqldf  ("select
                         Outflows_Delta,
                         first_response_6H,
                         first_response_24H
-                
+                        
                         
                         from BOP_data_diff")
 
@@ -336,19 +323,19 @@ people_data<- sqldf("select
                     Actual_Headcount,
                     Forecasted_Headcount
                     
-
+                    
                     from BOP_data_diff")
 people_data$Week<-as.Date(people_data$Week)
 
 SLA_data<- sqldf ("select
-                    Week,
-                    LOB,
-                    Actual_Outflows,
-                    first_response_6H,
-                    first_response_24H
-                    
-                    from BOP_data_diff
-                    where first_response_6H is not null")
+                  Week,
+                  LOB,
+                  Actual_Outflows,
+                  first_response_6H,
+                  first_response_24H
+                  
+                  from BOP_data_diff
+                  where first_response_6H is not null")
 SLA_data$Week<-as.Date(SLA_data$Week)
 
 
